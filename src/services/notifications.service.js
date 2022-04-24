7b71c78993bfd0d84eee54b1874d7c15d2e3c45e
Mikias Yonas <mikiasyonas512@gmail.com>
2022-04-24 15:53:40 +0300

    Finished the notification sending feature

--- src/services/notifications.service.js
+++ src/services/notifications.service.js
@@ -1,6 +1,10 @@
-const sendNotification = (notificationMessage, receiver, socket = null) => {
+const {emitWebsocketEvent} = require('./websocket.service');
+
+/* eslint-disable max-len */
+const sendNotification = (notificationObject, receiver, socket = null, type=null) => {
   switch (receiver.client) {
     case 'web':
+      emitWebsocketEvent(socket, receiver.id, notificationObject, type);
   }
 };
 
